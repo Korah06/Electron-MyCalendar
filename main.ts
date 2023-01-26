69d8f3a180f55777d50f0ac9d88d25b5bb2a424d
Mario <mario.espasa.2003@gmail.com>
2023-01-26 23:45:23 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,6 @@
+console.log(`Hello from Electron 👋`)
+
+export { app, BrowserWindow } from 'electron'
+
+const mainWindow
+
---
+++ src/components/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function main() {
+  return (
+    <div>
+      <h1>Hello from Electron renderer!</h1>
+      <p>👋</p>
+    </div>
+  );
+}
